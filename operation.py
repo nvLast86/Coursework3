b279571f661301957c711bdfbf6776cf6d225528
nvLast86 <n.lastenko@gmail.com>
2023-02-12 19:13:28 +0600

    operation.py with class Operation was created

---
+++ operation.py
@@ -0,0 +1,9 @@
+class Operation:
+
+    def __init__(self, opr_date, opr_state, opr_amount, opr_description, opr_from, opr_to):
+        self.opr_date = opr_date
+        self.opr_state = opr_state
+        self.opr_amount = opr_amount
+        self.opr_description = opr_description
+        self.opr_from = opr_from
+        self.opr_to = opr_to
