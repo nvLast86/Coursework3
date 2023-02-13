d8ec968dcf8bfa15d06116f247fd10883df6cff1
nvLast86 <n.lastenko@gmail.com>
2023-02-13 12:36:05 +0600

    Added docstrings

--- utils.py
+++ utils.py
@@ -3,12 +3,21 @@ from datetime import datetime
 
 
 def get_operation_info():
+    """
+    Получение списка всех операций из json файла, загруженного на сайт
+    :return: список всех операций
+    """
     response = requests.get('https://www.jsonkeeper.com/b/T1M3')
     operations = response.json()
     return operations
 
 
 def get_executed_operations_list(operations):
+    """
+    Получение списка успешно проведенных операций
+    :param operations: список всех операций
+    :return: список успешно проведенных операций
+    """
     exc_operations = []
     for operation in operations:
         if 'state' not in operation:
@@ -19,12 +28,22 @@ def get_executed_operations_list(operations):
 
 
 def sorted_exc_operations(operations):
+    """
+    Сортировка списка успешно проведенных операций, начиная с самой "свежей"
+    :param operations: список успешно проведенных операций
+    :return: отсортированный список успешно проведенных операций
+    """
     sorted_operations = sorted(operations,
                                key=lambda x: datetime.strptime(x['date'], '%Y-%m-%d %H:%M:%S'), reverse=True)
     return sorted_operations
 
 
 def transform_date(exc_time):
+    """
+    Извлечение даты проведения операции
+    :param exc_time: дата проведения операции в необработанном виде
+    :return: дата проведения операции в необходимом формате ГГГГ-ММ-ДД
+    """
     time = exc_time.split('T')
     time[1] = time[1][:8]
     time = ' '.join(time)
@@ -32,6 +51,11 @@ def transform_date(exc_time):
 
 
 def convert_date_format(some_date):
+    """
+    Преобразование даты в требуемый для вывода формат
+    :param some_date: дата проведения операции ГГГГ-ММ-ДД
+    :return: дата проведения операции ДД-ММ-ГГГГ
+    """
     date = some_date[:10]
     date = date.split('-')
     date.reverse()
@@ -40,6 +64,11 @@ def convert_date_format(some_date):
 
 
 def transform_and_hide_data(data):
+    """
+    Получение зашифрованной информации о транзакции с какого или на какой счет/карту
+    :param data: информация о счете или карты
+    :return: зашифрованная информация о счете или карте
+    """
     card_number = ''
     data_list = data.split(' ')
     if 'Счет' not in data_list:
@@ -59,6 +88,11 @@ def transform_and_hide_data(data):
 
 
 def get_hidden_card_number(card_number):
+    """
+    Шифрование номера карты
+    :param card_number: номер карты
+    :return: зашифрованный номер карты
+    """
     hide_card_number_temp = []
     for i in range(len(card_number)):
         if 7 <= i <= 14:
